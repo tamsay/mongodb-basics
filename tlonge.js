d0ac3641057670f94664177a783ea5bd4eba270a
tamsay <longetemitope@gmail.com>
2020-03-31 17:50:23 +0100

    Mongodb basics - first assignment

---
+++ tlonge.js
@@ -0,0 +1,10 @@
+const mongo = require('mongodb');
+
+const MongoClient = require('mongodb').MongoClient;
+let url = 'mongodb://localhost:27017/mydb';
+MongoClient.connect(url, function(err, db){
+    if(err) throw err;
+    console.log('Database created!');
+    db.close();
+});
+
